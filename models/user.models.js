9b4516e16a29b553d3f50ccef5860faf49815ccc
Baishnawi Agrawal <baishnawiagrawal08@gmail.com>
2024-10-29 16:07:12 +0545

    added required option

--- models/user.models.js
+++ models/user.models.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    userName: String,
-    email: String,
-    password: String,
-}, { collection: "usersData" });
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { collection: "usersData" }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
